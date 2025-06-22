8f60412126a235f2f0abcbe98dcf149607773eab
Himasnhukumawat <himanshunokhval@gmail.com>
2025-06-22 11:20:55 +0530

    firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,15 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  
+    apiKey: "AIzaSyCJ4JY0vjekg28It4y3v5pv-RWhVWzE6DU",
+    authDomain: "harshitageneralstore-3d00d.firebaseapp.com",
+    projectId: "harshitageneralstore-3d00d",
+    storageBucket: "harshitageneralstore-3d00d.firebasestorage.app",
+    messagingSenderId: "641816325196",
+    appId: "1:641816325196:web:14c01e5b8445ac376ad8e9",
+    measurementId: "G-0L46GS8PM3"
+ 
 }
 
 const app = initializeApp(firebaseConfig)
